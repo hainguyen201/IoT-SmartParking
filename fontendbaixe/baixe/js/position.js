43c2b1dbe275833b65ed8fa955a5f64b3ca01252
Anhduc <ducna241099@gmail.com>
2021-01-24 13:35:29 +0700

    son push

--- fontendbaixe/baixe/js/position.js
+++ fontendbaixe/baixe/js/position.js
@@ -25,6 +25,40 @@ function positionList() {
         type: 'GET',
         dataType: 'json',
         success: function(positions) {
+            var aTop ="A1";
+            var aBottom = "A2";
+            for (let i = 0; i < positions.length; i++) {
+                var position = positions[i].positionID;
+                var status   = positions[i].status;
+                if(position.indexOf(aTop) !== -1){
+                    if(status == 1){
+                        $("#A-top").append(
+                            `
+                                <span class="active">${position}</span>
+                            `);
+                    }
+                    else{
+                        $("#A-top").append(
+                            `
+                                <span>${position}</span>
+                            `);
+                    };
+                };
+                if(position.indexOf(aBottom) !== -1){
+                    if(status == 1){
+                        $("#a-bottom").append(
+                            `
+                            <span class="active">${position}</span>
+                            `);
+                    }
+                    else{
+                        $("#a-bottom").append(
+                            `
+                                <span>${position}</span>
+                            `);
+                    }
+                }
+             }
             console.log(positions);
             return positions;
         },
@@ -33,3 +67,4 @@ function positionList() {
         }
     });
 }
+positionList();
