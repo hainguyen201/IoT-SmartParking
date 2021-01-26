25c83ad4a433671ca8a363f7cd79af11c0778a7a
Anhduc <ducna241099@gmail.com>
2021-01-26 08:42:30 +0700

    update
    updtae

--- fontendbaixe/baixe/js/demo/chart-bar-demo.js
+++ fontendbaixe/baixe/js/demo/chart-bar-demo.js
@@ -26,86 +26,165 @@ function number_format(number, decimals, dec_point, thousands_sep) {
   }
   return s.join(dec);
 }
+function carList() {
+  var year;
+  $('select').on('change', function () {
+    year = this.value; 
+    $.ajax({
+      url: base_url + '/cars/',
+      type: 'GET',
+      dataType: 'json',
+      success: function(cars) {
+          var i = 0;
+          var thang1 = 0;
+          var thang2 = 0;
+          var thang3 = 0;
+          var thang4 = 0;
+          var thang5 = 0;
+          var thang6 = 0;
+          var thang7 = 0;
+          var thang8 = 0;
+          var thang9 = 0;
+          var thang10 = 0;
+          var thang11 = 0;
+          var thang12 = 0;
+          for (i = 0; i < cars.length; i++) {
+              var carCode = cars[i].CarCode;
+              var parkedTime = cars[i].ParkedTime;
+              var duration = cars[i].Duration;
+              var time = parkedTime.split(/\D+/);
+              if(time[0]==year){
+                if(time[1] == 1){
+                  thang1++;
+                }
+                if(time[1] == 2){
+                  thang2++;
+                }
+                if(time[1] == 3){
+                  thang3++;
+                }
+                if(time[1] == 4){
+                  thang4++;
+                }
+                if(time[1] == 5){
+                  thang5++;
+                }
+                if(time[1] == 6){
+                  thang6++;
+                }
+                if(time[1] == 7){
+                  thang7++;
+                }
+                if(time[1] == 8){
+                  thang8++;
+                }
+                if(time[1] == 9){
+                  thang8++;
+                }
+                if(time[1] == 10){
+                  thang10++;
+                }
+                if(time[1] == 11){
+                  thang12 ++;
+                }if(time[1] == 12){
+                  thang12++;
+                }
 
-// Bar Chart Example
-var ctx = document.getElementById("myBarChart");
-var myBarChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June","July","August", "September", "October", "November", "December"],
-    datasets: [{
-      label: "Revenue",
-      backgroundColor: "#4e73df",
-      hoverBackgroundColor: "#2e59d9",
-      borderColor: "#4e73df",
-      data: [4215, 5312, 6251, 7841, 9821, 14984,1234,7232,7234,7234,7451,7412],
-    }],
-  },
-  options: {
-    maintainAspectRatio: false,
-    layout: {
-      padding: {
-        left: 10,
-        right: 25,
-        top: 25,
-        bottom: 0
-      }
-    },
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'Day'
-        },
-        gridLines: {
-          display: false,
-          drawBorder: false
-        },
-        ticks: {
-          maxTicksLimit: 12
-        },
-        maxBarThickness: 25,
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 15000,
-          maxTicksLimit: 5,
-          padding: 10,
-          // Include a dollar sign in the ticks
-          callback: function(value, index, values) {
-            return  number_format(value);
+              }
+              
           }
-        },
-        gridLines: {
-          color: "rgb(234, 236, 244)",
-          zeroLineColor: "rgb(234, 236, 244)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
-        }
-      }],
-    },
-    legend: {
-      display: false
-    },
-    tooltips: {
-      titleMarginBottom: 10,
-      titleFontColor: '#6e707e',
-      titleFontSize: 14,
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-      callbacks: {
-        label: function(tooltipItem, chart) {
-          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + number_format(tooltipItem.yLabel);
-        }
+
+          var ctx = document.getElementById("myBarChart");
+          var myBarChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+              labels: ["January", "February", "March", "April", "May", "June","July","August", "September", "October", "November", "December"],
+              datasets: [{
+                label: "Revenue",
+                backgroundColor: "#4e73df",
+                hoverBackgroundColor: "#2e59d9",
+                borderColor: "#4e73df",
+                data: [thang1, thang2, thang3, thang4, thang5, thang6,thang7,thang8,thang9,thang10,thang11,thang12],
+              }],
+            },
+            options: {
+              maintainAspectRatio: false,
+              layout: {
+                padding: {
+                  left: 10,
+                  right: 25,
+                  top: 25,
+                  bottom: 0
+                }
+              },
+              scales: {
+                xAxes: [{
+                  time: {
+                    unit: 'Day'
+                  },
+                  gridLines: {
+                    display: false,
+                    drawBorder: false
+                  },
+                  ticks: {
+                    maxTicksLimit: 12
+                  },
+                  maxBarThickness: 25,
+                }],
+                yAxes: [{
+                  ticks: {
+                    min: 0,
+                    max: 150,
+                    maxTicksLimit: 5,
+                    padding: 10,
+                    // Include a dollar sign in the ticks
+                    callback: function(value, index, values) {
+                      return  number_format(value);
+                    }
+                  },
+                  gridLines: {
+                    color: "rgb(234, 236, 244)",
+                    zeroLineColor: "rgb(234, 236, 244)",
+                    drawBorder: false,
+                    borderDash: [2],
+                    zeroLineBorderDash: [2]
+                  }
+                }],
+              },
+              legend: {
+                display: false
+              },
+              tooltips: {
+                titleMarginBottom: 10,
+                titleFontColor: '#6e707e',
+                titleFontSize: 14,
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                caretPadding: 10,
+                callbacks: {
+                  label: function(tooltipItem, chart) {
+                    var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                    return datasetLabel + number_format(tooltipItem.yLabel);
+                  }
+                }
+              },
+            }
+          });
+
+      },
+      error: function(request, message, error) {
+          handleException(request, message, error);
       }
-    },
-  }
-});
+  });
+    carList();
+  });
+  console.log(year);
+
+};
+// Bar Chart Example
+carList();
